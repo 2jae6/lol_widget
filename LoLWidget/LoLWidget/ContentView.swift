8628bdb95bd5d31df38150d13419e7294625f0dd
LeeJaeWook <>
2022-01-29 23:37:41 +0900

    초기 프로젝트 설정

---
+++ LoLWidget/LoLWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoLWidget
+//
+//  Created by Jay on 2022/01/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoLWidget/LoLWidget/LoLWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoLWidgetApp.swift
+//  LoLWidget
+//
+//  Created by Jay on 2022/01/29.
+//
+
+import SwiftUI
+
+@main
+struct LoLWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
